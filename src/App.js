0a8e82e953db99cf742f135368e9c2a6958403bc
Sange203 <spr20priya@gmail.com>
2024-11-06 19:17:11 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Timer from './Timer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">
+       <h1>Press Start</h1>
+  
+      </header>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Timer.js
@@ -0,0 +1,47 @@
+ import {useEffect, useRef, useState} from "react";
+
+ function Timer(){
+
+    const[time,setTime]=useState(0)
+    const[running,setRunning]=useState(true)
+
+    const timer=useRef()
+
+    useEffect(()=>{
+        if(running){
+            timer.current=setInterval(()=>{
+                setTime(pre=>pre+1)
+            },1000)
+        }
+        return ()=>clearInterval(timer.current)
+    },[running])
+
+    return(
+      
+
+        <div className="stopwatch">
+            <p className="timer">{format(time)}</p>
+            <div className="actions">
+                <button style={{marginRight: '10px'}}  onClick={()=> setTime(0)}>Restart</button >
+                <button onClick={()=>{
+                    if(running) clearInterval(this.timer.current)
+                    setRunning(!running)
+                }}>{running ? 'Stop': 'Resume'}</button>
+            </div>
+        </div>
+    )
+}
+
+
+const format = (time)=>{
+    let hours=Math.floor(time/60/60%24)
+    let mintues=Math.floor(time/60%60)
+    let seconds=Math.floor(time%60)
+
+    hours=hours<10 ?'0'+hours : hours
+    mintues=mintues<10 ?'0'+mintues : mintues
+    seconds=seconds<10 ?'0'+seconds : seconds
+
+    return hours+ ": "+mintues+":"+seconds
+}
+export default Timer;
